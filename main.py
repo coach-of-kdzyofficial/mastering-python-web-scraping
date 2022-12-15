bd1ab8b9ba079af9ab7df13d2ccd44b23d3f69e5
Niepep <niepep72@gmail.com>
2022-12-15 20:08:13 +0700

    part2

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-import requests
+from requests import get    
+from bs4 import BeautifulSoup
+import json
 
-respons = requests.get('https://en.wikipedia.org/wiki/Khabib_Nurmagomedov')
+respons = get('https://en.wikipedia.org/wiki/Khabib_Nurmagomedov')
 print(respons.status_code)
-print(respons.text)
 
-with open('khabib_2.html', 'w', encoding='utf-8') as f:
-    f.write(respons.text)
+soup = BeautifulSoup(respons.text, 'html.parser')
+
+print(soup.title.string)
+tables = soup.find_all('table', attrs={'class':'wikitable'})
+matches = tables[1]
+trs = matches.find_all('tr')
+
+opponents = []
+
+for tr in trs:
+    tds = tr.find_all('td')
+    if not tds :
+        continue
+
+    opponents_node = tds[2]
+    opponents_name = opponents_node.string
+    if opponents_name is None:
+        opponents.append(opponents_name)
+        opponents_name = opponents_node.a.string
+
+    opponents.append(opponents_name.strip('\n'))
+
+print(opponents)
+
+opponents_json = json.dumps(opponents)
+print(opponents_json)
+
+with open('khabib_opponents.json', 'w', encoding='utf-8') as f:
+    f.write(opponents_json)
+   
+# with open('khabib_2.html', 'w', encoding='utf-8') as f:
+#     f.write(respons.text)
    
 
 
